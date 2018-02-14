88646d7b6f35d90ed864d97ba6a10ea496df6b34
Unknown <lucien.astie@gmail.com>
2018-02-14 21:32:08 +0100

    Getting rid of download-file dependency

--- getStories.js
+++ getStories.js
@@ -3,8 +3,9 @@ const {
   getMediaByCode,
   getUserByUsername
 } = require('instagram-stories');
+var url = require('url');
+var http = require('http');
 var fs = require('fs');
-var download = require('download-file');
 var req = require('request');
 var argv = require('minimist')(process.argv.slice(2));
 
@@ -13,6 +14,9 @@ const personalUserId = "";
 
 //get it in the cookies of your browser once you're connected to instagram on the computer
 const mySessionId = "";
+
+//Check if data exists or else i'm getting an error when trying to create subfolders
+checkDir("./data");
 
 if(argv.u){
   var username = argv.u;
@@ -29,7 +33,7 @@ if(argv.u){
           //elem_media type : 1 = picture, 2 = video
           if(elem.media_type == 1){
             if(!fs.existsSync(dataDir + elem.device_timestamp + ".jpg")){
-              download(elem.image_versions2.candidates[0].url,{directory : dataDir, filename : elem.device_timestamp + ".jpg"});
+              getFile(elem.image_versions2.candidates[0].url,username, elem.device_timestamp + ".jpg");
             } else {
               console.log("jpg already existing : " + elem.device_timestamp);
             }
@@ -37,7 +41,7 @@ if(argv.u){
 
           else if(elem.media_type == 2){
             if(!fs.existsSync(dataDir + elem.device_timestamp + ".mp4")){
-              download(elem.video_versions[0].url,{directory : dataDir, filename : elem.device_timestamp + ".mp4"});
+              getFile(elem.video_versions[0].url,username,elem.device_timestamp + ".mp4");
             } else {
               console.log("mp4 already existing : " + elem.device_timestamp);
             }
@@ -49,14 +53,14 @@ if(argv.u){
     if(argv.p) {
 
       //Replace some parts of the url to get full size picture
-      var url = instaObj.user.profile_pic_url_hd.replace("vp/","");
-      url = url.replace("s320x320","s1080x1080");
+      var urlPdp = instaObj.user.profile_pic_url_hd.replace("vp/","");
+      urlPdp = urlPdp.replace("s320x320","s1080x1080");
 
-      var pdpName = url.split('/');
+      var pdpName = urlPdp.split('/');
       pdpName = pdpName[pdpName.length - 1];
 
       if(!fs.existsSync( dataDir + "pdp_" + pdpName)){
-        download(url,{directory : dataDir, filename : "pdp_" + pdpName});
+        getFile(urlPdp,username,"pdp_" + pdpName);
       } else {
         console.log("pdp_" + pdpName + " is already downloaded");
       }
@@ -68,4 +72,30 @@ if(argv.u){
 
 if(argv.h){
   console.log("Help :\n     -u <username> : set the username of the account you want\n     -p : get profile picture\n     -s : get stories");
+}
+
+function getFile(urlFile, dir, name){
+  checkDir("./data/" + dir);
+  var opts = {
+    host:url.parse(urlFile).host,
+    port:80,
+    path: url.parse(urlFile).pathname
+  };
+  var file = fs.createWriteStream("./data/" + dir + "/" + name);
+  http.get(opts, function(res){
+    res.on('data', function(data){
+      file.write(data);
+    });
+    res.on('end', function(){
+      file.end();
+    });
+  });
+}
+
+function checkDir(dir) {
+  try {
+    fs.statSync(dir);
+  } catch(e) {
+    fs.mkdirSync(dir);
+  }
 }
